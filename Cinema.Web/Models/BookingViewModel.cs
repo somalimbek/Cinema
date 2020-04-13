8046f627ad4e31c3aec7a2a6f0101ddd91fdbd9c
Soma Limbek <soma.limbek@gmail.com>
2020-04-13 18:33:21 +0200

    Added script for choosing seats on booking page.

--- Cinema.Web/Models/BookingViewModel.cs
+++ Cinema.Web/Models/BookingViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,5 +15,17 @@ namespace Cinema.Web.Models
         public Movie Movie { get; set; }
 
         public Screen Screen { get; set; }
+
+        [Required(ErrorMessage = "Név megadása kötelező.")]
+        [MaxLength(50)]
+        public string CustomerName { get; set; }
+
+        [Required(ErrorMessage = "Telefonszám megadása kötelező.")]
+        [MaxLength(15, ErrorMessage = "Túl hosszú telefonszám.")]
+        public string CustomerPhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Nem választott helyeket.\nA foglalni kívánt székeket a fenti táblázatban, kattintással tudja kiválasztani.")]
+        [MaxLength(36, ErrorMessage = "Legfeljebb 6 hely foglalható.")]
+        public string SeatsToBook { get; set; }
     }
 }
