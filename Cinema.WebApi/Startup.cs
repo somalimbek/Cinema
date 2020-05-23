35fede387f9bfc41e36e5d6bd72ec394f985fe72
Soma Limbek <soma.limbek@gmail.com>
2020-05-23 12:15:47 +0200

    Added and configured WebApi.

---
+++ Cinema.WebApi/Startup.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cinema.Persistence;
+using Cinema.Persistence.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Cinema.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var dbType = Configuration.GetValue<DbType>("DbType");
+
+            switch (dbType)
+            {
+                case DbType.SqlServer:
+                    services.AddDbContext<CinemaContext>(options =>
+                        options.UseSqlServer(Configuration.GetConnectionString("SqlServerConnection")));
+                    break;
+                case DbType.Sqlite:
+                    services.AddDbContext<CinemaContext>(options =>
+                        options.UseSqlite(Configuration.GetConnectionString("SqliteConnection")));
+                    break;
+            }
+
+            services.AddTransient<CinemaService>();
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            DbInitializer.Initialize(serviceProvider, Configuration.GetValue<string>("ImageStore"));
+        }
+    }
+}
